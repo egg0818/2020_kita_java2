59113de672668022a8f14c1bff7ee5a13ec8d58d
taddy0818 <taddy0818@naver.com>
2020-07-03 12:43:38 +0900

    200703

--- src/com/koreait/blackjack/CardDeck.java
+++ src/com/koreait/blackjack/CardDeck.java
@@ -10,31 +10,59 @@ public class CardDeck {
 
 	public CardDeck() {
 		init();
+		
 	}
 
 	private void init() {
 		for (int i = 0; i < Card.PATTERNS.length; i++) {
 			for (int z = 1; z <= 13; z++) {
-				String deno;
-				switch (z) {
-				case 1:
-					deno = "A";
-					break;
-				case 11:
-					deno = "J";
-					break;
-				case 12:
-					deno = "Q";
-					break;
-				case 13:
-					deno = "K";
-					break;
-				default:
-					deno = String.valueOf(z);
-				}
-				arr.add(new Card(Card.PATTERNS[i],deno));
+				arr.add(new Card(Card.PATTERNS[i],getDeno(z)));
+//				String pattern = Card.PATTERNS[i];
+//				String deno = getDeno(z);
+//				Card c = new Card(pattern, deno);
+//				arr.add(c);
 			}
 		}
+	}
+	private String getDeno(int num) {
+		switch (num) {
+		case 1: return "A";
+		case 11: return "J";
+		case 12: return "Q";
+		case 13: return "K";
+		default: return String.valueOf(num);
+		}
+		
+	}
+	
+	//랜덤값 뽑아서 리턴. 카드덱에서 삭제
+	public Card getCard() {
+		int a = (int)(Math.random()*arr.size()); // 인덱스로 랜덤값 받기
+		Card temp = arr.get(a);
+		arr.remove(a);
+		return temp;
+		
+		
+	}
+	
+	/*
+	@Override 
+	public String toString() {
+		for(Card c : arr) {
+			System.out.println(c);
+		}
+		return "";
+	}
+	*/
+	
+	@Override
+	public String toString() {
+		String str="";
+		for(Card c : arr) {
+			str += (c + "\n");
+		}
+	
+	return str;
 	}
 
 }
--- src/com/koreait/blackjack/Game.java
+++ src/com/koreait/blackjack/Game.java
@@ -1,11 +1,19 @@
 package com.koreait.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
 	public static void main(String[] args) {
-		Card c1 = new Card("스페이드", "10");
 		CardDeck cd = new CardDeck();
+		System.out.println(cd); // 모든카드를 프린트
+		Card c = cd.getCard(); // 랜덤한 카드를 1장 리턴 and 카드덱에서 삭제
+		
+		
+		
 
 	}
-
+	
+	
 }
