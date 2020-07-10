e7277fdf82b720fcd844d5cf7614a0c108ac82a9
taddy0818 <taddy0818@naver.com>
2020-07-10 12:53:54 +0900

    200710

--- src/com/koreait/second/dao/OracleDAO.java
+++ src/com/koreait/second/dao/OracleDAO.java
@@ -5,28 +5,50 @@ import java.sql.DriverManager;
 
 public class OracleDAO {
 
-	public static void main(String[] args) {
-		OracleDAO o = new OracleDAO();
-		try {
-			o.getConn();
-		} catch (Exception e) {
-			System.out.println("오류");
+//	public static void main(String[] args) {
+//		OracleDAO o = new OracleDAO();
+//		try {
+//			o.getConn();
+//		} catch (Exception e) {
+//			System.out.println("오류");
+//		}
+//
+//	}
+	
+	
+	// 싱들톤 패턴! - 스프링땐 안적어도 자동으로 해줌
+	private static OracleDAO dao = null;
+	
+	private OracleDAO() {} // 기본 생성자. private 때문에 바깥에서 생성 불가능.
+	
+	public static OracleDAO getInstance() { 
+		if(dao == null) {
+			dao = new OracleDAO();
 		}
-
+		return dao; 
 	}
 
 	public Connection getConn() throws Exception { // 주의하고 외우셈
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 		String userName = "hr";
 		String password = "koreait2020";
+		Connection con = null;
 		
-
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection(url, userName, password); // 이거 외워야함 DrvierManger 외우셈!!!
+		con = DriverManager.getConnection(url, userName, password); // 이거 외워야함 DrvierManger 외우셈!!!
 
 		System.out.println("접속 성공");
 
 		return con;
 
+	}
+	
+	public void closeConn(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch(Exception e) {} 
+		}
 	}
 }
